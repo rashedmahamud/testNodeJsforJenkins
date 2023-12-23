e03721285ce5fe12e4303f0901c867a3c59e9d65
Rashed <rashedmahamud93@gmail.com>
2023-12-23 17:56:22 +0600

    Update node_app.js

--- node_app.js
+++ node_app.js
@@ -4,7 +4,7 @@ const http = require("http")
 // Creating server 
 const server = http.createServer((req, res) => { 
 	// Sending the response 
-	res.write("This is the response from the server") 
+	res.write("This is the response from the server. the app is running at port 6022") 
 	res.end(); 
 }) 
 
